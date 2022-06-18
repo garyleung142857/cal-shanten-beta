41bb1238c2d44c74e957e5559bfacf94ad53b87e
garyleung142857 <garyleung142857@gmail.com>
2022-06-18 15:56:43 +0800

    Add meta tag description

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import { router } from './router.js'
 import i18n from './lang'
 import vuetify from './plugins/vuetify'
 import VueI18n from 'vue-i18n'
+import VueMeta from 'vue-meta'
 
 Vue.config.productionTip = false
 Vue.use(Router)
 Vue.use(VueI18n)
+Vue.use(VueMeta)
 
 new Vue({
   vuetify,
